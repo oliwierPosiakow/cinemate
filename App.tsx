4375b87fbcfe1fc8c1742de1bd5b8297a7e7e6bd
Oliwier Posiakow <oliwierposiek@gmail.com>
2023-07-31 16:36:48 +0200

    Installed dependencies
    Added:
    - React Navigation (Stack)
    - React Native Paper

--- App.tsx
+++ App.tsx
@@ -1,5 +1,7 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native'
+
 
 export default function App() {
   return (
