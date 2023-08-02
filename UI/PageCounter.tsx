a5fdebfd570829c9d6954be83ea4162c03914b24
Oliwier Posiakow <oliwierposiek@gmail.com>
2023-08-03 00:03:33 +0200

    PageCounter styling

--- UI/PageCounter.tsx
+++ UI/PageCounter.tsx
@@ -1,29 +1,36 @@
 import {Button, Text} from "react-native-paper";
-import {FontAwesome5} from "@expo/vector-icons";
-import COLORS from "../const";
+import { MaterialIcons } from '@expo/vector-icons';import COLORS from "../const";
 import {TextInput, View, StyleSheet} from "react-native";
 import {useDispatch, useSelector} from "react-redux";
 import {decrement, increment} from "../redux/pageSlice";
+import {transparent} from "react-native-paper/lib/typescript/src/styles/themes/v2/colors";
 
 export default function PageCounter(){
     const dispatch = useDispatch()
     const page = useSelector((state) => state.page.value)
     return (
-        <View>
+        <View style={styles.counterWrapper}>
             <Button onPress={() => dispatch(decrement())}>
-                <FontAwesome5 name="minus" size={24} color={COLORS.text} />
+                <MaterialIcons name="navigate-before" size={20} color={COLORS.text} />
             </Button>
-            <Text style={styles.pageInput}>{page}</Text>
+            <Text variant={"headlineSmall"} style={styles.pageInput}>{page}</Text>
             <Button onPress={() => dispatch(increment())}>
-                <FontAwesome5 name="plus" size={24} color={COLORS.text} />
+                <MaterialIcons name="navigate-next" size={20} color={COLORS.text} />
             </Button>
         </View>
     )
 }
 const styles = StyleSheet.create({
+    counterWrapper:{
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: 10,
+        paddingVertical: 5
+    },
     pageInput:{
-        width: 50,
-        backgroundColor: 'white',
-        textAlign: "center"
+        backgroundColor: null,
+        color: COLORS.primary,
+        textAlign: "center",
     }
 })
